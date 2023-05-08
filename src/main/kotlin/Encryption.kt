ecf317f1f8675c423a508c30edf70d59c15e2030
CardosoDev04 <a50465@isel.alunos.pt>
2023-05-08 23:03:32 +0100

    Concatenation error fixed, hash is now shuffled

--- src/main/kotlin/Encryption.kt
+++ src/main/kotlin/Encryption.kt
@@ -1,3 +1,4 @@
+import java.nio.charset.Charset
 import java.security.MessageDigest
 import java.security.SecureRandom
 
@@ -31,8 +32,8 @@ fun generateSeed(length: Int): String{
 }
 
 fun winnerSeed(serverSeed: String, clientSeed: String): Int {
-    val hash = sha256(serverSeed,clientSeed)
-    val number = hash.substring(0,8).toLong(16) // Gets the first 8 characters (64 bit) of the hash and turns them into Long with base 16
+    val hash = sha256(serverSeed,clientSeed).toList().shuffled().joinToString("")
+    val number = hash.substring(0,8).toLong(16) // Gets 8 characters (64 bit) of the hash and turns them into Long with base 16
     return (number % 64).toInt()  // Divides the number by 64 and the rest of it's division is converted to int being that our winner number to set the color later
 }
 
